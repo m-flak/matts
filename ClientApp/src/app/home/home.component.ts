46e9caf60e145fc4a2ef4e9366bda560f0079134
Matthew Kehrer <matthew@kehrer.dev>
2023-04-03 18:12:38 -0500

    applicant component wip

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BackendService } from '../services/backend.service';
+import { Job } from '../models';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private _jobSubscription: Subscription | null = null;
+
+  jobs: Job[] = [];
+
+  constructor(private backendService: BackendService) {
+  }
+
+  ngOnDestroy(): void {
+    if (this._jobSubscription !== null) {
+      this._jobSubscription.unsubscribe();
+    }
+  }
+
+  ngOnInit(): void {
+    this._jobSubscription = this.backendService.getAllJobs().subscribe(theJobs => {
+      this.jobs = theJobs;
+    });
+  }
 }
