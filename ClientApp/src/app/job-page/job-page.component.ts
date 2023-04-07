653416c81f54f4db3961788af45e38f9cf3b4041
Matthew Kehrer <matthew@kehrer.dev>
2023-04-07 13:34:27 -0500

    schedule interview flow

--- ClientApp/src/app/job-page/job-page.component.ts
+++ ClientApp/src/app/job-page/job-page.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Applicant, Job } from '../models';
 import { Subscription, switchMap } from 'rxjs';
 import { BackendService } from '../services/backend.service';
+import { MonthViewDay } from 'calendar-utils';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-job-page',
@@ -10,7 +12,16 @@ import { BackendService } from '../services/backend.service';
   styleUrls: ['./job-page.component.scss']
 })
 export class JobPageComponent implements OnInit, OnDestroy {
+  readonly MODE_JOB_DETAILS: number = 0;
+  readonly MODE_INTERVIEW: number = 1;
+
   private _subscription: Subscription | null = null;
+
+  viewDate: Date = new Date();
+  mode = this.MODE_JOB_DETAILS;
+
+  @ViewChild('confirmationModal')
+  confirmationModal: any;
 
   @Input()
   currentJob: Job | null = null;
@@ -18,7 +29,7 @@ export class JobPageComponent implements OnInit, OnDestroy {
   @Input()
   currentApplicant: Applicant | null = null;
 
-  constructor(private activatedRoute: ActivatedRoute, private backendService: BackendService) { }
+  constructor(private activatedRoute: ActivatedRoute, private backendService: BackendService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this._subscription = 
@@ -26,6 +37,7 @@ export class JobPageComponent implements OnInit, OnDestroy {
         switchMap((params: ParamMap) => this.backendService.getJobDetails(Number(params.get('id'))))
       ).subscribe(data => {
         this.currentJob = data;
+        this.setMode(this.MODE_JOB_DETAILS);
       });
   }
 
@@ -37,5 +49,21 @@ export class JobPageComponent implements OnInit, OnDestroy {
 
   onPickApplicant(applicant: Applicant) {
     this.currentApplicant = applicant;
+  }
+
+  setMode(mode: number) {
+    this.mode = mode;
+  }
+
+  onDayClicked(data:{ day: MonthViewDay<any>; sourceEvent: MouseEvent | KeyboardEvent;}) {
+    const date = data.day.date;
+    this.viewDate = date;
+    this.confirmInterview();
+  }
+
+  confirmInterview() {
+    this.modalService.open(this.confirmationModal, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      console.log(this.viewDate);
+    });
   }
 }
