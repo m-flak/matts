804d6d16df6fb1642caa03a12351d48df4565364
Matthew Kehrer <matthew@kehrer.dev>
2023-07-19 21:13:13 -0500

    create token in authcontroller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -19,16 +19,22 @@ using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 [ApiController]
 [Route("[controller]")]
 public class AuthController : ControllerBase
 {
     private readonly ILogger<AuthController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public AuthController(ILogger<AuthController> logger) 
+    public AuthController(ILogger<AuthController> logger, IConfiguration configuration) 
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     [AllowAnonymous]
@@ -39,7 +45,25 @@ public class AuthController : ControllerBase
     [Route("login")]
     public IActionResult Login([FromBody] User user, [BindRequired] string requestedRole)
     {
-        _logger.LogInformation($"USER: {user.UserName} PW: {user.Password} ROLE: {requestedRole}");
-        return Unauthorized();
+        var issuer = _configuration["Jwt:Issuer"];
+        var audience = _configuration["Jwt:Audience"];
+        var signingKey = _configuration["Jwt:SigningKey"];
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("id", Guid.NewGuid().ToString()),
+                new Claim("role", requestedRole),
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }),
+            Expires = DateTime.UtcNow.AddDays(1),
+            Issuer = issuer,
+            Audience = audience,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Convert.FromBase64String(signingKey)), SecurityAlgorithms.HmacSha512Signature)
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return Ok(tokenHandler.WriteToken(token));
     }
 }
