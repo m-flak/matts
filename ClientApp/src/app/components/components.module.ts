a420d2ceb087544c6388d0b354735fa7d6c65aed
Matthew Kehrer <matthew@kehrer.dev>
2023-07-31 11:25:27 -0500

    start the employer toolbar component

--- ClientApp/src/app/components/components.module.ts
+++ ClientApp/src/app/components/components.module.ts
@@ -4,20 +4,37 @@ import {MatListModule} from '@angular/material/list';
 import { ApplicantsPickerComponent } from './applicants-picker/applicants-picker.component';
 import { ApplicantComponent } from './applicants-picker/applicant/applicant.component';
 import { JobListComponent } from './job-list/job-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatButtonModule } from '@angular/material/button';
+import { EmployerToolbarComponent } from '../components/employer-toolbar/employer-toolbar.component';
 
 @NgModule({
   declarations: [
     ApplicantsPickerComponent,
     ApplicantComponent,
-    JobListComponent
+    JobListComponent,
+    EmployerToolbarComponent
   ],
   imports: [
     CommonModule,
-    MatListModule
+    MatListModule,
+    HttpClientModule,
+    MatIconModule,
+    NgbToastModule,
+    MatButtonModule
   ],
   exports: [
     ApplicantsPickerComponent,
-    JobListComponent
+    JobListComponent,
+    EmployerToolbarComponent
   ]
 })
-export class ComponentsModule { }
+export class ComponentsModule { 
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('briefcase_list', sanitizer.bypassSecurityTrustResourceUrl('../../assets/briefcase-list.svg'));
+    iconRegistry.addSvgIcon('briefcase_new', sanitizer.bypassSecurityTrustResourceUrl('../../assets/briefcase-new.svg'));
+  }
+}
