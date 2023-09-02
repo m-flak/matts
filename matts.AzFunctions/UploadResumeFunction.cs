6b2a5ba2ef22c13f26ece5bc33a6c9d98051863a
Matthew Kehrer <matthew@kehrer.dev>
2023-09-02 11:50:33 -0500

    some setup

--- matts.AzFunctions/UploadResumeFunction.cs
+++ matts.AzFunctions/UploadResumeFunction.cs
@@ -39,7 +39,7 @@ public class UploadResumeFunction
     }
 
     [Function("UploadResumeFunction")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route="resumes/upload")] HttpRequestData req)
     {
         var parsedFormBody = await MultipartFormDataParser.ParseAsync(req.Body);
         string? fileName = parsedFormBody.GetParameterValue("fileName");
