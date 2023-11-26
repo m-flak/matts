8ac87fb6730a369621e0c16e9295c13429c431fe
Matthew Kehrer <matthew@kehrer.dev>
2023-11-25 23:48:57 -0600

    feat(create/#29): create skeleton function

---
+++ matts.AzFunctions/CreateTaskFunction.cs
@@ -0,0 +1,43 @@
+﻿/* matts
+ * "Matthew's ATS" - Portfolio Project
+ * Copyright (C) 2023  Matthew E. Kehrer <matthew@kehrer.dev>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace matts.AzFunctions;
+
+public class CreateTaskFunction
+{
+    private readonly ILogger _logger;
+
+    public CreateTaskFunction(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<CreateTaskFunction>();
+    }
+
+    [Function("CreateTaskFunction")]
+    public object Run([QueueTrigger("tasks", Connection = "AzureWebJobsStorage")] string taskJson)
+    {
+        _logger.LogInformation("Task: {}", taskJson);
+        return new object();
+    }
+}
