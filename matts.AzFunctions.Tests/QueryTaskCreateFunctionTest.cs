40e642d1423688a2d8ac5395d51738e5e2d3248b
Matthew Kehrer <matthew@kehrer.dev>
2023-11-30 16:56:45 -0600

    chore: setup tests project for functions

---
+++ matts.AzFunctions.Tests/QueryTaskCreateFunctionTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+using UnitTestEx.Xunit;
+
+namespace matts.AzFunctions.Tests;
+
+public class QueryTaskCreateFunctionTest : UnitTestBase
+{
+    public QueryTaskCreateFunctionTest(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
