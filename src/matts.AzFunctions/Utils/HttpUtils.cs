50c82d5d59faa8f7361c7a41d9cd11068d7d0f75
Matthew Kehrer <matthew@kehrer.dev>
2024-02-23 21:12:45 -0600

    chore(#33): QueueTaskCreateFunction Run() migrated

---
+++ src/matts.AzFunctions/Utils/HttpUtils.cs
@@ -0,0 +1,96 @@
+﻿/* matts
+ * "Matthew's ATS" - Portfolio Project
+ * Copyright (C) 2023-2024  Matthew E. Kehrer <matthew@kehrer.dev>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace matts.AzFunctions.Utils;
+
+internal sealed class HttpUtils
+{
+    internal static bool HandleCreateOptionsResponse(HttpRequest request, [MaybeNullWhen(false)] out OptionsOkResult response, string allowMethod = "POST")
+    {
+        if (string.Equals(request.Method, "OPTIONS", StringComparison.OrdinalIgnoreCase))
+        {
+            response = new OptionsOkResult(allowMethod);
+            return true;
+        }
+
+        response = null;
+        return false;
+    }
+
+    internal static bool IsContentType(HttpRequest request, string contentType)
+    {
+        bool hasHeader = request.Headers.TryGetValue("Content-Type", out var values);
+        bool matches = false;
+
+        if (hasHeader)
+        {
+            string value = values.FirstOrDefault(defaultValue: string.Empty) ?? string.Empty;
+            matches = string.Equals(contentType, value, StringComparison.OrdinalIgnoreCase);
+        }
+
+        return hasHeader && matches;
+    }
+
+    internal static ObjectResult ErrorResultWithDetails(
+                                    [Optional, DefaultParameterValue(HttpStatusCode.BadRequest)]
+                                        HttpStatusCode status,
+                                        string msg)
+    {
+        return new ObjectResult(
+            new
+            {
+                Message = msg
+            })
+        {
+            StatusCode = (int)status
+        };
+    }
+
+    [Obsolete(message: "Phasing out the Built-in Model")]
+    internal static bool HandleCreateOptionsResponse(HttpRequestData request, [MaybeNullWhen(false)] out HttpResponseData response)
+    {
+        if (string.Equals(request.Method, "OPTIONS", StringComparison.OrdinalIgnoreCase))
+        {
+            var optionsResponse = request.CreateResponse();
+            optionsResponse.Headers.Add("Allow", new[] { "POST", "OPTIONS" });
+            optionsResponse.Headers.Add("Access-Control-Allow-Origin", "*");
+            optionsResponse.StatusCode = HttpStatusCode.OK;
+            response = optionsResponse;
+            return true;
+        }
+
+        response = null;
+        return false;
+    }
+
+    [Obsolete(message: "Phasing out the Built-in Model")]
+    internal static async Task<HttpResponseData> CreateMessageResponseAsync(HttpRequestData request, HttpStatusCode code, string message)
+    {
+        var responseBad = request.CreateResponse(code);
+        await responseBad.WriteStringAsync(message);
+        return responseBad;
+    }
+
+    private HttpUtils() { }
+}
