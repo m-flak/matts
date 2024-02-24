efdee69a4ed6a99ab5e84120ef5ac109138b20f5
Matthew Kehrer <matthew@kehrer.dev>
2024-02-24 16:41:17 -0600

    chore(#33): complete QueueTaskCreateFunction piece

--- src/matts.AzFunctions.Tests/QueueTaskCreateFunctionTest.cs
+++ src/matts.AzFunctions.Tests/QueueTaskCreateFunctionTest.cs
@@ -11,6 +11,8 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
 using matts.AzFunctions.Utils;
+using Azure.Storage.Queues;
+using Azure;
 
 namespace matts.AzFunctions.Tests;
 
@@ -39,21 +41,24 @@ public class QueueTaskCreateFunctionTest : UnitTestBase
         using var test = CreateFunctionTester<Startup>();
         test.ConfigureServices(this.ConfigureLogging);
 
+        // Get route attribute value here because HttpTriggers can't run multiple methods in Run
         var route = test
             .Type<QueueTaskCreateFunction>()
             .Run(f => f.GetFunctionRoute())
             .Result;
 
+        // Mock and provide Func Context
         var context = new Mock<FunctionContext>();
         context.Setup(m => m.InstanceServices).Returns(test.Services);
 
+        // Generate request here because HttpTrigger can't run multiple methods in Run
         var request = test
             .CreateHttpRequest(HttpMethod.Options, route);
 
+        // Act & Assert
         var sut = await test
             .HttpTrigger<QueueTaskCreateFunction>()
             .RunAsync(f => f.Run(request, context.Object));
-
         var resp = sut
             .AssertOK()
             .Result
@@ -81,10 +86,10 @@ public class QueueTaskCreateFunctionTest : UnitTestBase
         var request = test
             .CreateHttpRequest(HttpMethod.Post, route, contentType: MediaTypeNames.Text.Plain);
 
+        // Act & Assert
         var sut = await test
             .HttpTrigger<QueueTaskCreateFunction>()
             .RunAsync(f => f.Run(request, context.Object));
-
         var resp = sut
             .AssertBadRequest()
             .Result
@@ -105,48 +110,86 @@ public class QueueTaskCreateFunctionTest : UnitTestBase
     [Fact]
     public async Task Function_Handles_SuccessfulProcessing()
     {
-        /*
-        var queue = new Mock<QueueClient>();
-        queue.Setup(q => q.CreateIfNotExistsAsync(It.IsAny<IDictionary<string, string>>(), It.IsAny<CancellationToken>()))
-            .Returns(Task.FromResult<Response?>(null));
-        _queueServiceClient.Setup(qc => qc.GetQueueClient(It.IsAny<string>()))
-            .Returns(queue.Object);
-
-        var qcResponse = new Mock<Response<SendReceipt>>();
-        queue.Setup(q => q.SendMessageAsync(It.IsAny<string>()))
-            .Returns(Task.FromResult(qcResponse.Object));
-
-        var request = Fixture.CreateRequestData(_functionContext, MediaTypeNames.Application.Json, "POST", Fixture.NewTaskRequestBody);
-
-        var sut = new QueueTaskCreateFunction(_loggerFactory, _queueServiceClient.Object, _schemaRegistry, _schemaPath);
-        var response = await sut.Run(request, _functionContext.Object);
-
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        */
         using var test = CreateFunctionTester<Startup>();
+        test.ConfigureServices(this.ConfigureLogging);
+
+        // Get route attribute value 
+        var route = test
+            .Type<QueueTaskCreateFunction>()
+            .Run(f => f.GetFunctionRoute())
+            .Result;
         var context = new Mock<FunctionContext>();
         context.Setup(m => m.InstanceServices).Returns(test.Services);
+
+        var request = test
+            .CreateHttpRequest(HttpMethod.Post, route, Fixture.NewTaskRequestBody, MediaTypeNames.Application.Json);
+
+        // Act & Assert
+        var sut = await test
+            .HttpTrigger<QueueTaskCreateFunction>()
+            .RunAsync(f => f.Run(request, context.Object));
+        var resp = sut
+            .AssertOK()
+            .Result
+            as JsonResult;
+        Assert.NotNull(resp);
+        Assert.NotNull(resp.Value);
+
+        var respBody = resp
+            .Value
+            .ToJsonDocument();
+
+        Assert.Equal((int)HttpStatusCode.OK, resp.StatusCode);
+        // check if the root json object isn't empty
+        Assert.True(respBody.RootElement.EnumerateObject().MoveNext());
     }
 
     [Fact]
-    public async Task Function_Handles_ErrorProcessing()
+    public async Task Function_Handles_ErrorProcessing_AzureFail()
     {
-        /*
+        using var test = CreateFunctionTester<Startup>();
+        test.ConfigureServices(this.ConfigureLogging);
+
+        // mock Azure QueueClient to fail
         var queue = new Mock<QueueClient>();
         queue.Setup(q => q.CreateIfNotExistsAsync(It.IsAny<IDictionary<string, string>>(), It.IsAny<CancellationToken>()))
             .Returns(Task.FromResult<Response?>(null));
-        _queueServiceClient.Setup(qc => qc.GetQueueClient(It.IsAny<string>()))
+        test.MockSingleton(queue);
+
+        // mock Azure QueueServiceCleint to provide the above mock
+        var queueServiceClient = new Mock<QueueServiceClient>();
+        queueServiceClient.Setup(qc => qc.GetQueueClient(It.IsAny<string>()))
             .Returns(queue.Object);
+        test.MockSingleton(queueServiceClient);
 
-        var request = Fixture.CreateRequestData(_functionContext, MediaTypeNames.Application.Json, "POST", Fixture.NewTaskRequestBody);
-
-        var sut = new QueueTaskCreateFunction(_loggerFactory, _queueServiceClient.Object, _schemaRegistry, _schemaPath);
-        var response = await sut.Run(request, _functionContext.Object);
-
-        Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
-        */
-        using var test = CreateFunctionTester<Startup>();
+        var route = test
+            .Type<QueueTaskCreateFunction>()
+            .Run(f => f.GetFunctionRoute())
+            .Result;
         var context = new Mock<FunctionContext>();
         context.Setup(m => m.InstanceServices).Returns(test.Services);
+
+        var request = test
+            .CreateHttpRequest(HttpMethod.Post, route, Fixture.NewTaskRequestBody, MediaTypeNames.Application.Json);
+
+        // Act & Assert
+        var sut = await test
+            .HttpTrigger<QueueTaskCreateFunction>()
+            .RunAsync(f => f.Run(request, context.Object));
+        var resp = sut
+            .Assert(HttpStatusCode.InternalServerError)
+            .Result
+            as ObjectResult;
+        Assert.NotNull(resp);
+        Assert.NotNull(resp.Value);
+
+        var respBody = resp
+            .Value
+            .ToJsonDocument();
+        string msg = respBody
+            .RootElement
+            .GetProperty("Message")
+            .GetRawText();
+        Assert.NotEmpty(msg);
     }
 }
