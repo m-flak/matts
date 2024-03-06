3c88e8890b76f9e7d6c0c70f45ee8098be3701d4
Matthew Kehrer <matthew@kehrer.dev>
2024-03-05 21:51:32 -0600

    feat(#30): function and test skeleton

---
+++ src/matts.AzFunctions.Tests/QueryLookupTaskFunctionTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using matts.AzFunctions.Tests.Helpers;
+using matts.AzFunctions.Utils;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using UnitTestEx.Xunit;
+using Xunit.Abstractions;
+
+namespace matts.AzFunctions.Tests;
+public class QueryLookupTaskFunctionTest : UnitTestBase
+{
+    private readonly ITestOutputHelper _output;
+
+    public QueryLookupTaskFunctionTest(ITestOutputHelper output)
+    : base(output)
+    {
+        _output = output;
+    }
+
+    private void ConfigureLogging(IServiceCollection services)
+    {
+        services.AddLogging(logBuilder =>
+            logBuilder
+                .SetMinimumLevel(LogLevel.Debug)
+                .AddXUnit(_output)
+                );
+    }
+
+    [Fact]
+    public async Task Function_RespondsTo_OPTIONS()
+    {
+        using var test = CreateFunctionTester<Startup>();
+        test.ConfigureServices(this.ConfigureLogging);
+
+        var route = FuncData.GetHttpRoute(typeof(QueryLookupTaskFunction));
+
+        // Mock and provide Func Context
+        var context = new Mock<FunctionContext>();
+        context.Setup(m => m.InstanceServices).Returns(test.Services);
+
+        // Act & Assert
+        var sut = await test
+            .HttpTrigger<QueryLookupTaskFunction>()
+            .RunAsync(f => f.Run(
+                    test.CreateHttpRequest(HttpMethod.Options, route),
+                    context.Object)
+            );
+        var resp = sut
+            .AssertOK()
+            .Result
+            as OptionsOkResult;
+        Assert.NotNull(resp);
+        Assert.Equal((int)HttpStatusCode.OK, resp.StatusCode);
+        Assert.Equivalent(new[] { "POST", "OPTIONS" }, resp.ResultHeaders["Allow"]);
+        Assert.Equal("*", resp.ResultHeaders["Access-Control-Allow-Origin"].First());
+    }
+}
---
+++ src/matts.AzFunctions/QueryLookupTaskFunction.cs
@@ -0,0 +1,51 @@
+﻿/* matts
+ * "Matthew's ATS" - Portfolio Project
+ * Copyright (C) 2023-2024  Matthew E. Kehrer <matthew@kehrer.dev>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+using Azure.Data.Tables;
+using matts.AzFunctions.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace matts.AzFunctions;
+
+public class QueryLookupTaskFunction
+{
+    private readonly ILogger<QueryLookupTaskFunction> _logger;
+    private readonly TableServiceClient _tableServiceClient;
+
+    public QueryLookupTaskFunction(ILogger<QueryLookupTaskFunction> logger, TableServiceClient tableServiceClient)
+    {
+        _logger = logger;
+        _tableServiceClient = tableServiceClient;
+    }
+
+    [Function("QueryLookupTaskFunction")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", "options", Route="tasks/query-lookup")] HttpRequest req, FunctionContext context)
+    {
+        req.EnableBuffering();
+
+        if (HttpUtils.HandleCreateOptionsResponse(req, out var optionsResponse))
+        {
+            return optionsResponse;
+        }
+
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
